52ed40632adb9b5d8fd714260c82cc45118b1356
MarwanBz <69892218+MarwanBz@users.noreply.github.com>
2023-03-04 18:21:47 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+  get perimeter() {
+    let sum = 0;
+    this.sides.forEach((side) => {
+      sum += side;
+    });
+    return sum;
+  }
+}
+
+class Triangle extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+  get isValid() {
+    if (
+      this.sides[0] + this.sides[1] > this.sides[2] &&
+      this.sides[1] + this.sides[2] > this.sides[0] &&
+      this.sides[0] + this.sides[2] > this.sides[1]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+class Square extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+  get isValid() {
+    if (
+      this.sides[0] === this.sides[1] &&
+      this.sides[1] === this.sides[2] &&
+      this.sides[2] === this.sides[3]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  get area() {
+    return this.sides[0] * this.sides[1];
+  }
+}
+
 // Your code here
